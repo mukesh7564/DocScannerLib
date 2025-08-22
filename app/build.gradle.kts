c0e26579b5cd3b1b78a00ab633dd73de737b1abc
mukeshkumar-27 <mukesh.kuamr@fourbrick.com>
2025-08-22 15:52:04 +0530

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,48 +1,51 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.library")
+    kotlin("android")
+    id("maven-publish")
 }
 
-
 android {
-    namespace = "com.mukesh.docscanner"
-    compileSdk = 35
+    namespace = "com.github.mukesh7564.docscanner"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.docscanner"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        minSdk = 21
+        targetSdk = 34
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
-
 dependencies {
-
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    implementation ("com.itextpdf:itextpdf:5.5.13.3")
+    implementation("com.itextpdf:itextpdf:5.5.13.3")
     implementation("androidx.camera:camera-core:1.3.1")
     implementation("androidx.camera:camera-camera2:1.3.1")
     implementation("androidx.camera:camera-lifecycle:1.3.1")
     implementation("androidx.camera:camera-view:1.3.1")
+}
 
+afterEvaluate {
+    extensions.getByType(org.gradle.api.publish.PublishingExtension::class.java).publications {
+        create<MavenPublication>("release") {
+            from(components["release"])
+            groupId = "com.github.mukesh7564"
+            artifactId = "DocScannerLib"
+            version = "1.0.0"
+        }
+    }
+
+    extensions.getByType(org.gradle.api.publish.PublishingExtension::class.java).repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/mukesh7564/DocScannerLib")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+                password = project.findProperty("gpr.token") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,12 +10,21 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
+    // Add plugin versions here
+    plugins {
+        id("com.android.application") version "8.2.1"
+        id("com.android.library") version "8.2.1"
+        kotlin("android") version "1.9.10"
+    }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
